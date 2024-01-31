8741dadd63f2d059935fde0be7bb9e129de5c309
Tilak Singh <tsingh99991@gmail.com>
2024-01-31 13:41:49 +0530

    Sign up page has made

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,21 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Navigation from "./components/Navigation";
-import Home from "./pages/Home";
 import Chat from "./pages/Chat";
 import Login from "./pages/Login";
-import Singup from "./pages/Signup";
+import Signup from "./pages/signup";
 
 function App() {
    return (
       <>
          <BrowserRouter>
-            <Navigation />
             <Routes>
-               <Route path="/" element={<Home />} />
                <Route path="/login" element={<Login />} />
-               <Route path="/signup" element={<Singup/>} />
-               <Route path="/chat" element={<Chat/>}/>
+               <Route path="/signup" element={<Signup/>} />
+               <Route path="/" element={<Chat/>}/>
             </Routes>
          </BrowserRouter>
       </>
